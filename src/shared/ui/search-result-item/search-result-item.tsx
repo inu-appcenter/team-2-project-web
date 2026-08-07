13124c797de2e69426d4e83fd494c4d133228f4f
milin3t <minjunseong@naver.com>
2026-08-08 01:29:31 +0900

    ♻️ refactor : 공통 UI 컴포넌트 폴더 구조 정리

--- src/shared/ui/search-result-item/search-result-item.tsx
+++ src/shared/ui/search-result-item/search-result-item.tsx
@@ -1,4 +1,4 @@
-import { Tag } from "./tag";
+import { Tag } from "../tag";
 
 export type SearchResultItemProps = {
   info?: string;
---
+++ src/shared/ui/selection/index.ts
@@ -0,0 +1,1 @@
+export { Checkbox, Radio, Toggle } from "./selection";
